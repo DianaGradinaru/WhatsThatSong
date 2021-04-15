2ae862440b36a2e4a951e9e987546978ea33f167
DianaGradinaru <diana.gradinaru.sincai@gmail.com>
2021-04-15 13:46:28 +0300

    parametrized functions

--- static/data.js
+++ static/data.js
@@ -1,18 +1,33 @@
-async function searchInLyrics() {
-    // event.preventDefault();
+const search_phrase = "never";
+
+const URL = `https://serene-garden-36285.herokuapp.com/http://api.musixmatch.com/ws/1.1/`;
+
+async function getLyricsByID(id) {
     const request = await fetch(
-        // "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=json&callback=callback&q_lyrics=imagine",
-        "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=json&callback=callback&q_track=imagine&apikey=ae8c1197ad5a0e25f83d21fda7650e63",
-        {
-            headers: {
-                "Content-type": "application/json",
-                "X-Auth-Token": "ae8c1197ad5a0e25f83d21fda7650e63",
-            },
-        }
+        `${URL}track.lyrics.get?track_id=${id}&apikey=ae8c1197ad5a0e25f83d21fda7650e63`
+    );
+    const response = await request.json();
+    if (request.ok) {
+        console.log(response.message.body.lyrics.lyrics_body);
+        return response.message.body.lyrics.lyrics_body;
+    }
+}
+
+async function getTrackIDByLyrics() {
+    const request = await fetch(
+        `${URL}track.search?q_lyrics=${search_phrase}s&apikey=ae8c1197ad5a0e25f83d21fda7650e63`
     );
     const response = await request.json();
 
-    console.log(response);
+    if (request.ok) {
+        const trackIds = response.message.body.track_list
+            .map((t) => t.track.track_id)
+            .map(getLyricsByID);
+        return trackIds;
+    }
 }
-
-searchInLyrics();
+const ids = getTrackIDByLyrics();
+ids.then((lyric) => {
+    console.log(lyric);
+});
+console.log(ids);
