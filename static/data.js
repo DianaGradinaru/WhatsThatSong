5d6cabe675ef58915bbebc55626e1c30f3e0279d
DianaGradinaru <diana.gradinaru.sincai@gmail.com>
2021-04-15 15:29:27 +0300

    Finished project

--- static/data.js
+++ static/data.js
@@ -1,33 +1,60 @@
-const search_phrase = "never";
+const form = document.querySelector("#searchform");
+const placeholder = document.getElementById("placeholder");
+const lyricscontainer = document.querySelector("#lyricscontainer");
+const URL =
+    "https://serene-garden-36285.herokuapp.com/http://api.musixmatch.com/ws/1.1/";
+const apikey = "ae8c1197ad5a0e25f83d21fda7650e63";
 
-const URL = `https://serene-garden-36285.herokuapp.com/http://api.musixmatch.com/ws/1.1/`;
+async function populateLyrics(track_id) {
+    const lyrics = await getLyricsByID(track_id);
+    lyricscontainer.innerText = lyrics;
+}
+
+function createTitles(track, i) {
+    const p = document.createElement("p");
+    const a = document.createElement("a");
+    a.innerText = track.track_name;
+    a.href = "#";
+    a.addEventListener("click", (e) => {
+        e.preventDefault();
+        populateLyrics(track.track_id);
+    });
+    p.appendChild(a);
+    placeholder.appendChild(p);
+}
 
 async function getLyricsByID(id) {
     const request = await fetch(
-        `${URL}track.lyrics.get?track_id=${id}&apikey=ae8c1197ad5a0e25f83d21fda7650e63`
+        `${URL}track.lyrics.get?track_id=${id}&apikey=${apikey}`
     );
-    const response = await request.json();
     if (request.ok) {
-        console.log(response.message.body.lyrics.lyrics_body);
+        const response = await request.json();
         return response.message.body.lyrics.lyrics_body;
     }
 }
 
-async function getTrackIDByLyrics() {
+async function getTrackIDByLyrics(phrase) {
     const request = await fetch(
-        `${URL}track.search?q_lyrics=${search_phrase}s&apikey=ae8c1197ad5a0e25f83d21fda7650e63`
+        `${URL}track.search?q_lyrics=${phrase}s&apikey=${apikey}`
     );
-    const response = await request.json();
 
     if (request.ok) {
-        const trackIds = response.message.body.track_list
-            .map((t) => t.track.track_id)
-            .map(getLyricsByID);
-        return trackIds;
+        const response = await request.json();
+        // trackIds = response.message.body.track_list
+        //     .map((t) => t.track.track_id)
+        //     .map(async (i) => getLyricsByID(i));
+
+        trackName = response.message.body.track_list
+            .map((n) => n.track)
+            .map(createTitles);
+
+        return trackName;
     }
 }
-const ids = getTrackIDByLyrics();
-ids.then((lyric) => {
-    console.log(lyric);
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    placeholder.innerHTML = "";
+    lyricscontainer.innerHTML = "";
+    getTrackIDByLyrics(e.target.phrase.value);
 });
-console.log(ids);
